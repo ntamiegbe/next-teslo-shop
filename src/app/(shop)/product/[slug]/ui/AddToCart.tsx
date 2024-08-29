489551456a4399cc6b3076964a99d0ab6180337e
Ntami Egbe <77502425+ntamiegbe@users.noreply.github.com>
2024-08-29 11:30:43 +0100

    fix:

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -4,6 +4,9 @@ import { useState } from "react";
 import { QuantitySelector } from "@/components";
 import type { CartProduct, Product } from "@/interfaces";
 import { useCartStore } from '@/store';
+import { Plus, ShoppingCart } from "lucide-react";
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
 interface Props {
   product: Product;
@@ -13,6 +16,7 @@ export const AddToCart = ({ product }: Props) => {
   const addProductToCart = useCartStore( state => state.addProductTocart );
   const [quantity, setQuantity] = useState<number>(1);
   const [posted, setPosted] = useState(false);
+  const router = useRouter();
 
   const addToCart = () => {
     setPosted(true);
@@ -31,13 +35,49 @@ export const AddToCart = ({ product }: Props) => {
     setQuantity(1);
   };
 
+  const placeOrder = () => {
+    setPosted(true);
+
+    const cartProduct: CartProduct = {
+      id: product.id,
+      slug: product.slug,
+      title: product.title,
+      price: product.price,
+      quantity,
+      image: product.images[0]
+    }
+
+    addProductToCart(cartProduct);
+    router.push('/cart');
+
+  }
+
   return (
     <>
       <QuantitySelector quantity={quantity} onQuantityChanged={setQuantity} />
 
-      <button onClick={addToCart} className="btn-primary my-5">
-        Add to cart
-      </button>
+      <div className="flex items-center gap-x-4 justify-between">
+        <motion.button
+          onClick={placeOrder}
+          disabled={posted}
+          className="my-3 w-full flex items-center justify-center border border-[#1e186f] text-[#1e186f] bg-white font-bold py-2 px-4 rounded-lg transition-colors duration-300"
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          <Plus size={20} className="mr-2" />
+          Order now
+        </motion.button>
+        <motion.button
+          onClick={addToCart}
+          disabled={posted}
+          className="my-3 w-full flex items-center justify-center bg-[#1e186f] text-white font-bold py-2 px-4 rounded-lg transition-colors duration-300"
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.95 }}
+        >
+          <ShoppingCart size={20} className="mr-2" />
+          Add to Cart
+        </motion.button>
+      </div>
     </>
   );
 };
--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -46,23 +46,6 @@ export default function ProductGridItem({ product }: Props) {
     }, 2000);
   };
 
-  const placeOrder = () => {
-    setPosted(true);
-
-    const cartProduct: CartProduct = {
-      id: product.id,
-      slug: product.slug,
-      title: product.title,
-      price: product.price,
-      quantity,
-      image: product.images[0]
-    }
-
-    addProductToCart(cartProduct);
-    router.push('/cart');
-    
-  }
-
   return (
     <>
       <div
@@ -112,28 +95,16 @@ export default function ProductGridItem({ product }: Props) {
           <div className="mt-3">
             <QuantitySelector quantity={quantity} onQuantityChanged={setQuantity} />
           </div>
-          <div className="flex items-center gap-x-4 justify-between">
-            <motion.button
-              onClick={placeOrder}
-              disabled={posted}
-              className="my-3 w-full flex items-center justify-center border border-[#1e186f] text-[#1e186f] bg-white font-bold py-2 px-4 rounded-lg transition-colors duration-300"
-              whileHover={{ scale: 1.02 }}
-              whileTap={{ scale: 0.95 }}
-            >
-              <Plus size={20} className="mr-2" />
-              Order now
-            </motion.button>
-            <motion.button
-              onClick={addToCart}
-              disabled={posted}
-              className="my-3 w-full flex items-center justify-center bg-[#1e186f] text-white font-bold py-2 px-4 rounded-lg transition-colors duration-300"
-              whileHover={{ scale: 1.02 }}
-              whileTap={{ scale: 0.95 }}
-            >
-              <ShoppingCart size={20} className="mr-2" />
-              Add to Cart
-            </motion.button>
-          </div>
+          <motion.button
+            onClick={addToCart}
+            disabled={posted}
+            className="my-3 w-full flex items-center justify-center bg-[#1e186f] text-white font-bold py-2 px-4 rounded-lg transition-colors duration-300"
+            whileHover={{ scale: 1.02 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <ShoppingCart size={20} className="mr-2" />
+            Add to Cart
+          </motion.button>
         </div>
 
         {isHovered && (
